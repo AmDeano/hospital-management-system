e74f76f0a715629a64e34954974de320e283ffdc
Abdelilah Melhaoui <153937839+AmDeano@users.noreply.github.com>
2025-08-10 02:34:31 +0100

    Correct API in ControllerTest Class V7

--- hospital-management-system/employee_service/src/main/java/com/hospital/employee/entity/EmployeeType.java
+++ hospital-management-system/employee_service/src/main/java/com/hospital/employee/entity/EmployeeType.java
@@ -3,7 +3,7 @@ package com.hospital.employee.entity;
 /**
  * Enumeration for different types of employees in the hospital system
  */
-public enum EmployeeType {
+/*public enum EmployeeType {
 	
     ADMINISTRATION("Administration"),
     MEDICAL_STAFF("Medical Staff");
@@ -21,5 +21,38 @@ public enum EmployeeType {
     @Override
     public String toString() {
         return displayName;
+    }
+}*/
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum EmployeeType {
+
+    ADMINISTRATION("Administration"),
+    MEDICAL_STAFF("Medical Staff");
+
+    private final String displayName;
+
+    EmployeeType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @JsonValue
+    public String toJson() {
+        return name();
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
+
+    @JsonCreator
+    public static EmployeeType fromJson(String value) {
+        return EmployeeType.valueOf(value.toUpperCase());
     }
 }
--- hospital-management-system/employee_service/src/main/java/com/hospital/employee/entity/WorkDay.java
+++ hospital-management-system/employee_service/src/main/java/com/hospital/employee/entity/WorkDay.java
@@ -3,7 +3,7 @@ package com.hospital.employee.entity;
 /**
  * Enumeration for work days of the week
  */
-public enum WorkDay {
+/*public enum WorkDay {
     MONDAY("Monday"),
     TUESDAY("Tuesday"),
     WEDNESDAY("Wednesday"),
@@ -31,7 +31,7 @@ public enum WorkDay {
      * Check if the day is a weekend day
      * @return true if Saturday or Sunday
      */
-    public boolean isWeekend() {
+    /*public boolean isWeekend() {
         return this == SATURDAY || this == SUNDAY;
     }
 
@@ -39,6 +39,53 @@ public enum WorkDay {
      * Check if the day is a weekday
      * @return true if Monday through Friday
      */
+    /*public boolean isWeekday() {
+        return !isWeekend();
+    }
+}*/
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum WorkDay {
+    MONDAY("Monday"),
+    TUESDAY("Tuesday"),
+    WEDNESDAY("Wednesday"),
+    THURSDAY("Thursday"),
+    FRIDAY("Friday"),
+    SATURDAY("Saturday"),
+    SUNDAY("Sunday");
+
+    private final String displayName;
+
+    WorkDay(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    // Serialize enum as its name() (e.g. MONDAY)
+    @JsonValue
+    public String toJson() {
+        return name();
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
+
+    // Optional: for deserialization from string to enum
+    @JsonCreator
+    public static WorkDay fromJson(String value) {
+        return WorkDay.valueOf(value.toUpperCase());
+    }
+
+    public boolean isWeekend() {
+        return this == SATURDAY || this == SUNDAY;
+    }
+
     public boolean isWeekday() {
         return !isWeekend();
     }
